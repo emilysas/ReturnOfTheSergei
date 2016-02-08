4a58de0a982674310acf3e4036ebe87eb80f6853
Emily Sas <emily.sas@comparethemarket.com>
2016-02-08 16:31:02 +0000

    Adding C# Test and Code Projects

---
+++ StarWarsKata/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWarsKata
+{
+    public class Class1
+    {
+    }
+}
---
+++ StarWarsTests/Tests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace StarWarsTests
+{
+    [TestFixture]
+    public class Tests
+    {
+    }
+}
